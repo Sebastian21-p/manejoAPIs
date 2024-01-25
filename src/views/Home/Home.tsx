a3fc67fb477ec0fc3ee3fe4ffb6f17217ee21f15
Sebastian Bermon <jesussebastianbber@ufps.edu.co>
2024-01-24 19:22:49 -0500

    Imagen del dia funciona

--- src/views/Home/Home.tsx
+++ src/views/Home/Home.tsx
@@ -1,24 +1,33 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { View, StyleSheet } from "react-native";
 import Header from "../../components/Header";
 import fetchApi from "../../utils/fetch";
+import TodayImages from "../../components/TodayImages";
+import { PostImage } from "../../types";
+
 
 const Home = () => {
+  const [todaysImage, setTodaysImage] = useState<PostImage>();
+
   useEffect(() => {
     const loadTodaysImage = async () => {
       try {
-        const todaysImage = await fetchApi();
-        console.log(todaysImage);
+        const todaysImageResponse = await fetchApi();
+        setTodaysImage(todaysImageResponse);
       } catch (error) {
         console.log(error);
+        setTodaysImage({});
       }
     };
     loadTodaysImage().catch(null);
   }, []);
   
+console.log(todaysImage);
+
   return (
     <View style={styles.container}>
       <Header />
+      <TodayImages {...todaysImage} />
     </View>
   );
 };
